b7a8000c68653a76fb0f0d21a879ea0f32e4d829
dipyamanroy <d1pyamanroy@protonmail.com>
2024-09-23 19:39:50 +0530

    Updated upcoming event

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,11 +2,18 @@ import { Button } from "./ui/button";
 import { buttonVariants } from "./ui/button";
 import { HeroCards } from "./HeroCards";
 import { DiscordLogoIcon } from "@radix-ui/react-icons";
+import { Badge } from "@/components/ui/badge";
 
 export const Hero = () => {
   return (
     <section className="container grid lg:grid-cols-2 place-items-center py-20 md:py-32 gap-10">
       <div className="text-center lg:text-start space-y-6">
+        <Badge variant="outline" className="text-sm py-2">
+          <span className="mr-2 text-primary">
+            <Badge>Upcoming</Badge>
+          </span>
+          <span> REBOOT Orientation, Sep 24 </span>
+        </Badge>
         <main className="text-5xl md:text-6xl font-bold">
           <h1 className="inline">
             
--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -15,6 +15,8 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
+        live:
+          "border-transparent bg-green-500 text-white hover:bg-green-500/80 animate-pulse",
       },
     },
     defaultVariants: {
