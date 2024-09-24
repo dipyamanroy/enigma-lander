b062a11a0090db4b0c5830479fcb1591fbde14af
dipyamanroy <d1pyamanroy@protonmail.com>
2024-09-24 12:12:20 +0530

    Fixed Vercel analytics integration.

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,12 @@ import { ScrollToTop } from "./components/ScrollToTop";
 import { Services } from "./components/Services";
 import { Sponsors } from "./components/Sponsors";
 import { Team } from "./components/Team";
-import { Analytics } from "@vercel/analytics/react"
 // import { Testimonials } from "./components/Testimonials";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Analytics/>
       <Navbar />
       <Hero />
       <Sponsors />
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import { ThemeProvider } from "@/components/theme-provider.tsx";
+import App from "./App";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
     </ThemeProvider>
+    <Analytics />
   </React.StrictMode>
 );
